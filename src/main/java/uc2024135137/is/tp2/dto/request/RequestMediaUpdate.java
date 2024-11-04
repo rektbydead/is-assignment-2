9444efc0495998724f60c35f79753907ad93cd93
Ruben Lousada <56137190+rektbydead@users.noreply.github.com>
2024-11-04 22:30:14 +0000

    Update RequestMediaUpdate.java

--- src/main/java/uc2024135137/is/tp2/dto/request/RequestMediaUpdate.java
+++ src/main/java/uc2024135137/is/tp2/dto/request/RequestMediaUpdate.java
@@ -1,7 +1,9 @@
 package uc2024135137.is.tp2.dto.request;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import uc2024135137.is.tp2.model.Media;
 
 import java.util.Date;
 
@@ -10,7 +12,8 @@ public class RequestMediaUpdate {
 
     private String title;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
 
-    private String mediaType;
+    private Media.Type mediaType;
 }
