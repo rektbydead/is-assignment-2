2c497f2149b5c4082596b901cbb7d701cf2a0784
Ruben Lousada <56137190+rektbydead@users.noreply.github.com>
2024-11-04 22:30:01 +0000

    Update RequestMediaCreate.java

--- src/main/java/uc2024135137/is/tp2/dto/request/RequestMediaCreate.java
+++ src/main/java/uc2024135137/is/tp2/dto/request/RequestMediaCreate.java
@@ -4,6 +4,7 @@ package uc2024135137.is.tp2.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
+import uc2024135137.is.tp2.model.Media;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -16,5 +17,5 @@ public class RequestMediaCreate {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate releaseDate;
 
-    private String mediaType;
+    private Media.Type mediaType;
 }
